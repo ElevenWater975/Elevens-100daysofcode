fdcdfe59f48f10aa0b41510830f7b0094aa8ca1a
ElevenWater975 <65044666+ElevenWater975@users.noreply.github.com>
2021-05-13 12:16:29 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
 #Eleven's python project for the 100 days of code challenge
+
+print("There once was a man named George")
+print("he was 70 years old")
+print("He really liked the name George")
+print("But he didn't like being 70.")
