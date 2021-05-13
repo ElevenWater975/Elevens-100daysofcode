8863c8f1434d1dd1188639a73f5b9458b2e27a42
ElevenWater975 <65044666+ElevenWater975@users.noreply.github.com>
2021-05-13 13:16:01 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 #Eleven's python project for the 100 days of code challenge
 #Always seperate variable names with an underscore
 
-character_name = "Tom"
-character_age = "50"
-print("There once was a man named " + character_name + ", ")
-print("he was " + character_age + " years old")
-
-character_name = "Mike"
-print("He really liked the name " + character_name + ", ")
-print("But didn't like being " + character_age + ". ")
+name = input("Please enter your name: ")
+password = input("Please enter a password: ")
+print("Welcome " + name + "!") 
