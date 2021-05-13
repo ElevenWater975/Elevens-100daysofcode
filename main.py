dfdb7cd9c818253d64708c5f8d70674926292074
ElevenWater975 <65044666+ElevenWater975@users.noreply.github.com>
2021-05-13 12:49:43 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 #Eleven's python project for the 100 days of code challenge
+#Always seperate variable names with an underscore
 
-print("There once was a man named George")
-print("he was 70 years old")
-print("He really liked the name George")
-print("But he didn't like being 70.")
+character_name = "Tom"
+character_age = "50"
+print("There once was a man named " + character_name + ", ")
+print("he was " + character_age + " years old")
+
+character_name = "Mike"
+print("He really liked the name " + character_name + ", ")
+print("But didn't like being " + character_age + ". ")
